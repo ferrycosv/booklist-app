0b46dd9388f9727bca063b63f58c81e71789e98a
Francisco Fernández <ferrycosv@gmail.com>
2020-04-02 01:51:45 +0200

    Form input validation and alert display of a failed or success operation

--- handler.js
+++ handler.js
@@ -5,16 +5,28 @@ var handler = {
     const title = document.querySelector("#title").value;
     const author = document.querySelector("#author").value;
     const isbn = document.querySelector("#isbn").value;
-    // Instatiate book
-    const book = new Book(title, author, isbn);
-    // Add Book to UI
-    UI.addBookToList(book);
-    // Clear fields
-    UI.clearFields();
+    // Validate
+    if (title === "" || author === "" || isbn === "") {
+      UI.showAlert("Please fill in all fields", "danger");
+    } else {
+      // Instatiate book
+      const book = new Book(title, author, isbn);
+
+      // Add Book to UI
+      UI.addBookToList(book);
+
+      // Show success message
+      UI.showAlert("Book Added", "success");
+
+      // Clear fields
+      UI.clearFields();
+    }
   },
   deleteBook: function(event) {
     // Remove book from UI
     UI.deleteBook(event.target);
+    // Show success message
+    UI.showAlert("Book Removed", "success");
   }
 };
 // Event: Display Books
